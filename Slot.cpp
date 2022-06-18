a1750cea907d00eaa5715182aaa4ab965f0836ab
NataliaLopezO <106278490+NataliaLopezO@users.noreply.github.com>
2022-06-17 22:54:13 -0500

    Add files via upload

---
+++ Slot.cpp
@@ -0,0 +1,36 @@
+/*
+  Archivo: Slot.cpp
+  Autor: Natalia Lopez Osorio
+  Email: natalia.lopez.osorio@correounivalle.edu.co
+  Fecha creación: 2022-05
+  Fecha última modificación: 2022-05
+*/
+
+#include "Slot.h" 
+
+Slot::Slot(string posicion, int valor)
+{
+  this->posicion=posicion;
+  this->valor=valor;
+ 
+}
+
+Slot::~Slot()
+{
+  //No hace nada
+}
+
+int Slot::obtenerValor()
+{
+  return valor;
+}
+  
+string Slot::obtenerPosicion()
+{
+  return posicion;
+}
+
+void Slot::cambiarValor(int nuevoValor)
+{
+  this->valor = nuevoValor;
+}
